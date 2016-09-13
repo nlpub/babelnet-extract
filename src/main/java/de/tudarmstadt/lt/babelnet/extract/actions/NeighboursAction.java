26b2ac41f26c4e12c4c65c731931597b95e9055d
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2016-09-13 21:27:08 +0200

    Add the Done message in NeighboursAction

--- src/main/java/de/tudarmstadt/lt/babelnet/extract/actions/NeighboursAction.java
+++ src/main/java/de/tudarmstadt/lt/babelnet/extract/actions/NeighboursAction.java
@@ -56,6 +56,8 @@ public class NeighboursAction {
                     }
                 })
         );
+
+        logger.log(Level.INFO, "Done");
     }
 
     private Map<String, Integer> walk(BabelSynset source) throws IOException {
