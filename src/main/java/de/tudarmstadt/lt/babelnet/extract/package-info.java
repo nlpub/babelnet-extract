bb674d0e2580b3033a4954e1d744cf363eaee4f5
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2016-09-14 15:30:27 +0200

    Add BabelNet links to documentation [ci skip]

--- src/main/java/de/tudarmstadt/lt/babelnet/extract/package-info.java
+++ src/main/java/de/tudarmstadt/lt/babelnet/extract/package-info.java
@@ -1,4 +1,6 @@
 /**
  * BabelNet Extract is an application for extracting certain data from the BabelNet lexical ontology.
+ *
+ * @see <a href="http://babelnet.org/">http://babelnet.org/</a>
  */
 package de.tudarmstadt.lt.babelnet.extract;
